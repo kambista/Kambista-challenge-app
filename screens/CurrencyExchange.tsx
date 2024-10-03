42ef9eec4b045263821392e89a85938c41cb0ec0
AlexJohan97 <avelino.johan@pucp.edu.pe>
2024-10-03 00:45:29 -0500

    Global Context with Zustand

--- screens/CurrencyExchange.tsx
+++ screens/CurrencyExchange.tsx
@@ -1,12 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { useStore } from "../store/store";
 
 export default function CurrencyExchange() {
+  function BearCounter() {
+    const bears = useStore((state: any) => state.bears);
+    return <Text>{bears} around here...</Text>;
+  }
   return (
     <View>
       <Text>CurrencyExchange</Text>
       <Text>CurrencyExchange</Text>
       <Text>CurrencyExchange</Text>
       <Text>CurrencyExchange</Text>
+      <TouchableOpacity>{BearCounter()}</TouchableOpacity>
     </View>
   );
 }
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,11 +1,28 @@
 import { useNavigation } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
+import { useStore } from "../store/store";
 // import useCurrencyStore from "../../store/store";
 
 export default function Login() {
   const nav = useNavigation();
   //   const currency = useCurrencyStore((state) => state.currency);
   //   const setCurrency = useCurrencyStore((state) => state.setCurrency);
+  function BearCounter() {
+    const bears = useStore((state: any) => state.bears);
+    return <Text>{bears} around here...</Text>;
+  }
+
+  function Controls() {
+    const increasePopulation = useStore(
+      (state: any) => state.increasePopulation
+    );
+    return (
+      <TouchableOpacity onPress={increasePopulation}>
+        <Text>one up</Text>
+      </TouchableOpacity>
+    );
+  }
+
   return (
     <View>
       <Text>Login</Text>
@@ -29,6 +46,8 @@ export default function Login() {
       >
         <Text>CurrencyExchange</Text>
       </TouchableOpacity>
+      <TouchableOpacity>{BearCounter()}</TouchableOpacity>
+      <TouchableOpacity>{Controls()}</TouchableOpacity>
     </View>
   );
 }
