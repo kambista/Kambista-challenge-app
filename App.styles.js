d2ae6cf329d51ea694d4c576750d809d4ef0a954
AlexJohan97 <avelino.johan@pucp.edu.pe>
2024-10-03 01:19:17 -0500

    eas build, kambista logo

--- App.styles.js
+++
@@ -1,5 +0,0 @@
-// styles.ts
-export const containerStyles = "flex-[1] bg-yellow-500 pt-8 ";
-export const textStyles = "text-3xl text-red-500 font-bold";
-export const buttonStyles =
-  "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
